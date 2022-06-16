34e4af544fb5743dbf256c9072f2627873bffcd4
Rillian Grant <rillian.grant@gmail.com>
2022-06-16 02:40:50 +0100

    .

--- stdsplit/stdsplit-installer.sh
+++ stdsplit/stdsplit-installer.sh
@@ -8,7 +8,7 @@ if [ $? -ne 0 ]; then
     exit 1
 fi
 
-Destination=$(dialog --keep-tite --title "Choose a destination for the binary:" --dselect $PWD 24 80 3>&1 1>&2 2>&3 3>&-)
+Destination=$(dialog --keep-tite --inputbox "Choose a destination directory for the binary:" 24 80 $PWD 3>&1 1>&2 2>&3 3>&-)
 if [ $? -ne 0 ]; then
     dialog --keep-tite --msgbox "Installation aborted." 0 0
     exit 1
@@ -16,8 +16,10 @@ fi
 
 touch $Destination/stdsplit && chmod +x $Destination/stdsplit
 if [ ! $? -eq 0 ]; then
-    dialog --keep-tite --msgbox "Installation aborted. $Destination is not writable" 0 0
+    dialog --keep-tite --msgbox "Installation aborted. $Destination/stdsplit is not writable" 0 0
     exit 1
 fi
 
 cat $0 | grep "^#bin" | sed -e 's/^#bin//' | base64 -d > $Destination/stdsplit
+
+dialog --keep-tite --msgbox "Success, the executable has been written to: $Destination/stdsplit" 0 0
